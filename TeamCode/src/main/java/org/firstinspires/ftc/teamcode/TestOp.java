5fccd177c44a5ae7ecb8dc0ceea1e9dc19dc77e2
Joshua030608 <64506906+Joshua030608@users.noreply.github.com>
2025-10-11 10:39:33 -0400

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOp.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.hardware.Servo;
+import com.rframeworks.di.Inject;
+import com.rframeworks.di.Injector;
+import com.rframeworks.di.Resolved;
+import com.rframeworks.eventbus.*;
+import com.rframeworks.di.Injectable;
+
+import com.rframeworks.di.generated.GeneratedRegistry;
+
+import org.firstinspires.ftc.teamcode.mocks.MockOp;
+import org.firstinspires.ftc.teamcode.mocks.MockRegistry;
+import org.firstinspires.ftc.teamcode.mocks.OpModeMock;
+import org.firstinspires.ftc.teamcode.mocks.Servo.ServoMock;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@MockOp(name = "Test OpMode", group = "OpModes")
+@Disabled
+public class TestOp extends OpModeMock {
+    EventBus<String> eb = EventBus.getInstance();
+//    Servo
+
+//    Resolved
+//    Class1 c1;
+
+    public static void Register() {
+        MockRegistry.register(TestOp.class);
+    }
+
+    @Override
+    public void init() {
+        GeneratedRegistry.registerAll();
+        System.out.println("Init!");
+
+        Class1 c1 = new Class1();
+        c1.printSomethingFromTest();
+
+//        this.test.printSomething();
+    }
+
+    @Override
+    public void init_loop() {
+        this.pressPlay();
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Started!");
+        this.pressStop();
+    }
+
+    @Override
+    public void loop() {
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stopped");
+    }
+}
