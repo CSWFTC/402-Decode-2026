61f4f94ecbc5afc54515f3efdc5a3742ca9d7c5a
Arnav Choubey <choubey.arnav@charterschool.org>
2025-11-15 12:50:29 -0500

    Debounce/denoise joystick inputs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -16,10 +16,24 @@ public class DriverControl extends LinearOpMode {
         Shooter shooter = new Shooter();
         waitForStart();
         Gamepad old = new Gamepad();
+        double strafe = 0, forward = 0, rotate = 0;
+        long lastJoystickTime = 0;
         while(opModeIsActive()){
-            double forward = gamepad1.left_stick_y;
-            double strafe = gamepad1.left_stick_x;
-            double rotate = gamepad1.right_stick_x;
+            if(System.currentTimeMillis() > lastJoystickTime + 20){
+                boolean newPosition = (gamepad1.left_stick_x != strafe) ||
+                        (gamepad1.left_stick_y != forward) ||
+                        (gamepad1.right_stick_x != rotate);
+
+                // Check for Joystick at Rest
+                boolean atRest = (gamepad1.left_stick_x == 0f) && (gamepad1.left_stick_y == 0f) &&
+                        (gamepad1.right_stick_x == 0f);
+                if (newPosition || !atRest) {
+                    lastJoystickTime = System.currentTimeMillis();
+                    strafe = gamepad1.left_stick_x;
+                    forward = gamepad1.left_stick_y;
+                    rotate = gamepad1.right_stick_x;
+                }
+            }
             double scale = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(rotate), 1);
             Hardware.frontLeft.setPower((forward + strafe + rotate) / scale);
             Hardware.frontRight.setPower((forward - strafe - rotate) / scale);
