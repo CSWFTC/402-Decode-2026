1fa035dd6bb2c13743090f2f444e1b402396d00d
Arnav Choubey <choubey.arnav@charterschool.org>
2025-11-19 14:46:22 -0500

    Move execution to individual opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Simulation/OpModeBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Simulation/OpModeBase.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.Simulation;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 public class OpModeBase extends LinearOpMode {
+    public static OpModeBase Init(){
+        return new OpModeBase();
+    }
     @Override
     public void runOpMode(){
         Run(Constants.createFollower(hardwareMap));
@@ -13,5 +17,8 @@ public class OpModeBase extends LinearOpMode {
 
     public void Run(Follower follower){
 
+    }
+    public static void main(String[] args){
+        Init().Run(new Follower(new FollowerConstants(), new SimLocalizer(), new SimDrivetrain()));
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraightLineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraightLineTest.java
@@ -7,8 +7,12 @@ import com.pedropathing.geometry.Pose;
 import org.firstinspires.ftc.teamcode.Simulation.OpModeBase;
 
 public class StraightLineTest extends OpModeBase {
+    public static StraightLineTest Init(){
+        return new StraightLineTest();
+    }
     @Override
     public void Run(Follower follower) {
+        System.out.println("Starting straight line test");
         follower.followPath(follower.pathBuilder().addPath(new BezierLine(new Pose(0, 0), new Pose(0, 12))).build());
     }
 }
