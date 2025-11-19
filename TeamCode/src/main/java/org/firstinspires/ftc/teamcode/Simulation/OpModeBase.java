788e8b6a33fd2cfbb0d6232fa9e7a59828558bb6
Arnav Choubey <choubey.arnav@charterschool.org>
2025-11-19 15:25:33 -0500

    Make it an abstract class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Simulation/OpModeBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Simulation/OpModeBase.java
@@ -6,18 +6,16 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
-public class OpModeBase extends LinearOpMode {
-    public static OpModeBase Init(){
-        return new OpModeBase();
+public abstract class OpModeBase extends LinearOpMode {
+    public static OpModeBase Init() {
+        return null;
     }
+
     @Override
     public void runOpMode(){
         Run(Constants.createFollower(hardwareMap));
     }
-
-    public void Run(Follower follower){
-
-    }
+    public abstract void Run(Follower follower);
     public static void main(String[] args){
         Init().Run(new Follower(new FollowerConstants(), new SimLocalizer(), new SimDrivetrain()));
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraightLineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraightLineTest.java
@@ -10,7 +10,6 @@ public class StraightLineTest extends OpModeBase {
     public static StraightLineTest Init(){
         return new StraightLineTest();
     }
-    @Override
     public void Run(Follower follower) {
         System.out.println("Starting straight line test");
         follower.followPath(follower.pathBuilder().addPath(new BezierLine(new Pose(0, 0), new Pose(0, 12))).build());
