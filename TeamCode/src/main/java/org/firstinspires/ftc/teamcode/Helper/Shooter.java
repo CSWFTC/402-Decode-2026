bd760624d2f77a777215b9bc5c7a39ef24530ea6
Arnav Choubey <choubey.arnav@charterschool.org>
2025-11-24 18:20:23 -0500

    runtime config of shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Shooter.java
@@ -1,6 +1,13 @@
 package org.firstinspires.ftc.teamcode.Helper;
 
+import com.bylazar.configurables.annotations.Configurable;
+
+@Configurable
 public class Shooter {
+    public static double intakePower = 1;
+    public static double outtakeTopPower = 0.5;
+    public static double outtakeMiddlePower = 1;
+    public static double outtakeBottomPower = 1;
     public boolean intakeOn;
     public boolean outtakeOn;
     public Shooter(){
@@ -10,13 +17,13 @@ public class Shooter {
 
     private void SetIntake(boolean status) {
         intakeOn = status;
-        Hardware.intake.setPower(status ? 1.0 : 0.0);
+        Hardware.intake.setPower(status ? intakePower : 0.0);
     }
     private void SetOuttake(boolean status) {
         outtakeOn = status;
-        Hardware.outtakeBottom.setPower(status ? 1.0 : 0.0);
-        Hardware.outtakeMiddle.setPower(status ? 1.0 : 0.0);
-        Hardware.outtakeTop.setPower(status ? 1.0 : 0.0);
+        Hardware.outtakeBottom.setPower(status ? outtakeBottomPower : 0.0);
+        Hardware.outtakeMiddle.setPower(status ? outtakeMiddlePower : 0.0);
+        Hardware.outtakeTop.setPower(status ? outtakeTopPower : 0.0);
     }
 
     public void ToggleIntake() {
