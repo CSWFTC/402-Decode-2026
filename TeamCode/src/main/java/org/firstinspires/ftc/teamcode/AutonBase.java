412a7849ad608b21fb27283939870c7dd3be28de
Arnav Choubey <choubey.arnav@charterschool.org>
2026-02-24 19:52:06 -0500

    the rest of the auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonBase.java
@@ -8,23 +8,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Helper.Hardware;
 import org.firstinspires.ftc.teamcode.Helper.Shooter;
-import org.firstinspires.ftc.teamcode.Helper.Turret;
 import org.firstinspires.ftc.teamcode.PedroPathing.Constants;
 
 enum State {
     LAUNCHING_PRELOAD,
-    GOING_TO_FIRST,
-    PICKING_UP_FIRST,
-    GOING_TO_LAUNCH_FIRST,
-    LAUNCHING_FIRST,
-    GOING_TO_SECOND,
-    PICKING_UP_SECOND,
-    GOING_TO_LAUNCH_SECOND,
-    LAUNCHING_SECOND,
-    GOING_TO_THIRD,
-    PICKING_UP_THIRD,
-    GOING_TO_LAUNCH_THIRD,
-    LAUNCHING_THIRD,
+    GOING_TO_PICKUP,
+    PICKING_UP,
+    GOING_TO_LAUNCH,
+    LAUNCHING,
     LEAVE
 }
 
@@ -48,45 +39,59 @@ public class AutonBase extends LinearOpMode {
     @Override
     public void runOpMode() {
         Hardware.init(hardwareMap);
-        Turret t = new Turret();
         Shooter shooter = new Shooter();
         State s = State.LAUNCHING_PRELOAD;
         Follower f = Constants.createFollower(hardwareMap);
         f.setStartingPose(launchRear);
         waitForStart();
-        PathChain goto1 = pathBetween(f, launchRear, pickup1);
-        PathChain collect1 = pathBetween(f, pickup1, offset(pickup1));
-        PathChain launch1 = pathBetween(f, offset(pickup1), launchRear);
-        // TODO: Start Launching
+        PathChain[] goTo = {pathBetween(f, launchRear, pickup1), pathBetween(f, launchRear, pickup2), pathBetween(f, launchForward, pickup3)};
+        PathChain[] collect = {pathBetween(f, pickup1, offset(pickup1)), pathBetween(f, pickup2, offset(pickup2)), pathBetween(f, pickup3, offset(pickup3))};
+        PathChain[] launch = {pathBetween(f, offset(pickup1), launchRear), pathBetween(f, offset(pickup2), launchForward), pathBetween(f, offset(pickup3), launchForward)};
+        PathChain leave = pathBetween(f, launchForward, endPosition);
+        int set = 0;
+        shooter.SetOuttake(true); // TODO: may want to replace with continuously running outtake and instead toggling transfer
         while (opModeIsActive()) {
             f.update();
             switch (s) {
                 case LAUNCHING_PRELOAD:
-                    // TODO: Stop Launching at Some Point
-                    s = State.GOING_TO_FIRST;
-                    f.followPath(goto1);
+                    // TODO: Wait to be done launching
+                    shooter.SetOuttake(false);
+                    s = State.GOING_TO_PICKUP;
+                    f.followPath(goTo[0]);
                     break;
-                case GOING_TO_FIRST:
+                case GOING_TO_PICKUP:
                     if (!f.isBusy()) {
-                        s = State.PICKING_UP_FIRST;
-                        f.followPath(collect1);
+                        s = State.PICKING_UP;
+                        shooter.SetIntake(true);
+                        f.followPath(collect[set]);
                     }
                     break;
-                case PICKING_UP_FIRST:
+                case PICKING_UP:
                     if (!f.isBusy()) {
-                        s = State.GOING_TO_LAUNCH_FIRST;
-                        f.followPath(launch1, true);
+                        s = State.GOING_TO_LAUNCH;
+                        shooter.SetIntake(false);
+                        f.followPath(launch[set], true);
                     }
                     break;
-                case GOING_TO_LAUNCH_FIRST:
+                case GOING_TO_LAUNCH:
                     if (!f.isBusy()) {
-                        // TODO: Start Launching
-                        s = State.LAUNCHING_FIRST;
+                        shooter.SetOuttake(true);
+                        s = State.LAUNCHING;
                     }
                     break;
-                case LAUNCHING_FIRST:
-                    // TODO: Stop Launching at Some Point
-                    s = State.GOING_TO_SECOND;
+                case LAUNCHING:
+                    // TODO: Wait to be done launching
+                    shooter.SetOuttake(false);
+                    if (++set < 3) {
+                        s = State.GOING_TO_PICKUP;
+                        f.followPath(goTo[set]);
+                    } else {
+                        s = State.LEAVE;
+                        f.followPath(leave, true);
+                    }
+                    break;
+                case LEAVE:
+                    // idk if we need to do anything here
                     break;
             }
         }
