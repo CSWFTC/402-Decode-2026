be2048b79d386f2342adbe5033aec1082ff290e7
Dhanushb <109532712+Dhanushb2028@users.noreply.github.com>
2026-02-28 17:03:26 -0500

    ig bro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRed.java
@@ -11,9 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 @Configurable
 @TeleOp(name = "RED auton")
 public class AutonRed extends AutonBase {
-    //public static Config c = new Config(); // TODO: add definitions for all the values
-    //THIS C IS INCORRECT, this is the values for AUTONRED, this needs to be changed
-    public static Config c = new AutonBase.Config();
+    public static Config c = new AutonBase.Config(new Pose(61.599, 19.187), new Pose(86.269, 85.520),new Pose(58.928, 34.456), new Pose(58.928, 59.962),new Pose(58.928, 85.411),new Pose(-39.6157082749,0),new Pose(86.286, 85.499), new Pose2D(DistanceUnit.INCH,131.781, 133.961, AngleUnit.DEGREES, 20));
     @Override
     Config getConfig() {
         return c;
