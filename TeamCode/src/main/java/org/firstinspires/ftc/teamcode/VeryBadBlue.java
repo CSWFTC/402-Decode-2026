d40b042b7305d098aff8af1d5a9b1a6e30036769
Arnav Choubey <choubey.arnav@charterschool.org>
2026-02-28 09:53:32 -0500

    two really bad auton opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VeryBadBlue.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Helper.Hardware;
+import org.firstinspires.ftc.teamcode.Helper.Shooter;
+
+@Autonomous(name = "VERY BAD BLUE AUTON")
+public class VeryBadBlue extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        Shooter s = new Shooter();
+        Hardware.frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Hardware.frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Hardware.rearLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Hardware.rearRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        waitForStart();
+        s.SetOuttake(true);
+        sleep(10000);
+        Hardware.frontLeft.setPower(-1);
+        Hardware.rearLeft.setPower(-1);
+        sleep(100);
+        Hardware.frontRight.setPower(-1);
+        Hardware.rearRight.setPower(-1);
+        sleep(250);
+        Hardware.frontRight.setPower(0);
+        Hardware.frontLeft.setPower(0);
+        Hardware.rearLeft.setPower(0);
+        Hardware.rearLeft.setPower(0);
+    }
+}
